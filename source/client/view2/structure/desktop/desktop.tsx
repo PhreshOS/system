bd43c20de4f73c97e72beefe1b33e163e6e2fdd6
zohayrslileh <zohayrslileh@gmail.com>
2026-08-27 00:52:10 +0100

    Create desktop layer structure

--- source/client/view2/structure/desktop/desktop.tsx
+++ source/client/view2/structure/desktop/desktop.tsx
@@ -1,4 +1,22 @@
+import Wallpaper from "./wallpaper/wallpaper"
+import UnderWindows from "./under-windows/under-windows"
+import Windows from "./windows/windows"
+import OverWindows from "./over-windows/over-windows"
+import Taskbar from "./taskbar/taskbar"
+
 export default function () {
 
-    return null
+    return <div className="grid grid-rows-5 p-3">
+
+        <Wallpaper />
+
+        <UnderWindows />
+
+        <Windows />
+
+        <OverWindows />
+
+        <Taskbar />
+
+    </div>
 }
---
+++ source/client/view2/structure/desktop/under-windows/under-windows.tsx
@@ -0,0 +1,4 @@
+export default function () {
+
+    return <div className="bg-surface/20" />
+}
