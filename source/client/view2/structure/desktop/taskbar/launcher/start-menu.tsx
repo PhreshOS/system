e9fbab1517cf23d43b05f2d945f990ce0947ad02
zohayrslileh <zohayrslileh@gmail.com>
2026-08-27 02:32:02 +0100

    Use Tailwind for start menu geometry

--- source/client/view2/structure/desktop/taskbar/launcher/start-menu.tsx
+++ source/client/view2/structure/desktop/taskbar/launcher/start-menu.tsx
@@ -1,25 +1,16 @@
 import { Surface } from "@heroui/react"
-import { CSSProperties } from "react"
-
-const position = {
-
-    positionAnchor: "--desktop-taskbar",
-
-    top: "auto",
-
-    right: "auto",
-
-    bottom: "auto",
-
-    left: "auto",
-
-    insetBlockEnd: "calc(anchor(top) + var(--spacing) * 2.5)",
-
-    insetInlineStart: "anchor(start)"
-
-} satisfies CSSProperties
 
 export default function ({ id }: { id: string }) {
 
-    return <Surface id={id} popover="auto" aria-label="Start Menu" style={position} className="m-0 size-80 rounded-lg border-0 p-0 outline-none">{null}</Surface>
+    return <Surface
+
+        id={id}
+
+        popover="auto"
+
+        aria-label="Start Menu"
+
+        className="inset-auto inset-be-[calc(anchor(top)+var(--spacing)*2.5)] inset-s-[anchor(start)] m-0 size-80 rounded-lg border-0 p-0 outline-none [position-anchor:--desktop-taskbar]"
+
+    >{null}</Surface>
 }
