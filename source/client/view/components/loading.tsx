989228ded0cb0b60a0f06a579b054d7650f1ff28
zohayrslileh <zohayrslileh@gmail.com>
2026-09-18 01:20:54 +0100

    save

--- source/client/view/components/loading.tsx
+++ source/client/view/components/loading.tsx
@@ -1,24 +1,34 @@
-import { Surface, type SurfaceProps } from "@phreshos/react-ui"
+import { Surface, useAppearance, useThemedValue, type SurfaceProps } from "@phreshos/react-ui"
 import Spinner from "./spinner"
 
 type LoadingProps = Omit<SurfaceProps, "material">
 
 /** A complete loading surface for unresolved content. */
 export default function ({ className, style, ...props }: LoadingProps) {
+    const background = useThemedValue(useAppearance().colors).background
 
-    return <Surface
+    return <div
 
-        {...props}
-
-        material={{ opacity: 1 }}
-
-        className={`inset-0 z-10 grid ${className ?? ""}`}
+        className={`inset-0 z-10 ${className ?? ""}`}
 
         style={{ ...style, position: "absolute", borderRadius: style?.borderRadius ?? "inherit" }}
 
     >
 
-        <Spinner className="m-auto size-6" />
+        <div
+            aria-hidden="true"
+            style={{ position: "absolute", inset: 0, borderRadius: "inherit", backgroundColor: background }}
+        />
 
-    </Surface>
+        <Surface
+            {...props}
+            className="absolute inset-0 grid"
+            style={{ borderRadius: "inherit" }}
+        >
+
+            <Spinner className="m-auto size-6" />
+
+        </Surface>
+
+    </div>
 }
